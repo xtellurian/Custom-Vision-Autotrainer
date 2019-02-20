21939e117303ca29aa8ca187ea4432def5e74637
Rian Finnegan <rian.finnegan@microsoft.com>
2019-02-20 14:47:57 +1100

    create a list of upload by url

--- src/autotrainer/autotrainer/custom_vision/custom_vision_client.py
+++ src/autotrainer/autotrainer/custom_vision/custom_vision_client.py
@@ -1,9 +1,12 @@
 
 from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
-from azure.cognitiveservices.vision.customvision.training.models import Project
+from azure.cognitiveservices.vision.customvision.training.models import Project, ImageUrlCreateEntry, Tag
 
 from autotrainer.custom_vision.domains import Domain
 from autotrainer.custom_vision.classification_type import ClassificationType
+from autotrainer.custom_vision.labeller import Labeller
+
+from autotrainer.blob.blob_client import LabelledBlob
 
 class CustomVisionClient:
     training_client: CustomVisionTrainingClient
@@ -18,3 +21,14 @@ class CustomVisionClient:
             domain_id=domain.to_id(),
             classification_type=classification_type.to_id())
         return project
+
+    def create_image_url_list(self, project: Project, labelled_blobs: [LabelledBlob])-> [ImageUrlCreateEntry]:
+        labeller = Labeller()
+        image_url_create_list = []
+        for labelled_blob in labelled_blobs:
+            tag_ids = []
+            for label in labelled_blob.labels:
+                tag_ids.append(labeller.add_label_if_not_exists(self.training_client, project, label).id)
+            image_url_create_list.append( ImageUrlCreateEntry(url=labelled_blob.download_url, tag_ids=tag_ids ))
+        return image_url_create_list
+
--- src/autotrainer/custom_vision_tests.py
+++ src/autotrainer/custom_vision_tests.py
@@ -2,7 +2,9 @@
 import os
 import unittest
 from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
-from azure.cognitiveservices.vision.customvision.training.models import Project
+from azure.cognitiveservices.vision.customvision.training.models import Project, ImageUrlCreateEntry
+
+from autotrainer.blob.blob_client import LabelledBlob
 
 from autotrainer.custom_vision.custom_vision_client import CustomVisionClient
 from autotrainer.custom_vision.domains import Domain
@@ -12,8 +14,12 @@ CVTK=os.environ['CUSTOMVISIONTRAININGKEY']
 training_client = CustomVisionTrainingClient(CVTK, 'https://australiaeast.api.cognitive.microsoft.com')
 
 class CustomVisionTests(unittest.TestCase):
+    project: Project
+    def setUp(self):
+        self.project = training_client.create_project('unit-tests')
     
-    # def setUp(self):
+    def tearDown(self):
+        training_client.delete_project(self.project.id)
 
     def test_create_project(self):
         client = CustomVisionClient(training_client)
@@ -24,3 +30,12 @@ class CustomVisionTests(unittest.TestCase):
         projects = training_client.get_projects()
         self.assertIn(project, projects)
         training_client.delete_project(project.id)
+
+    def test_create_image_url_list(self):
+        client = CustomVisionClient(training_client)
+        labelled_blobs = [LabelledBlob('url1', ['tomato','potato']), LabelledBlob('url2', ['banana','fig'])]
+        image_urls = client.create_image_url_list(self.project, labelled_blobs )
+        for labelled_blob in labelled_blobs:
+            self.assertIn(labelled_blob.download_url, [i.url for i in image_urls])
+        for image in image_urls:
+            self.assertIsInstance(image, ImageUrlCreateEntry)
